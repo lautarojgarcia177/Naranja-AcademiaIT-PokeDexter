eef24a86c33b65131dc71ad21905f482b3fa6d3e
Lautaro J Garcia <lautarojgarcia177@gmail.com>
2021-04-29 00:11:47 -0300

    pokemon name first letter capital

--- src/app/pokemon.service.spec.ts
+++ src/app/pokemon.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Pokemon } from './pokemon.interface';
 import { PokemonService } from './pokemon.service';
+import ditto from './ditto.json';
+
+let httpClientSpy: { get: jasmine.Spy };
 
 describe('PokemonService', () => {
   let service: PokemonService;
@@ -8,9 +11,23 @@ describe('PokemonService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PokemonService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return expected pokemon (HttpClient called once)', () => {
+    const expectedPokemon: Pokemon = ditto;
+
+    httpClientSpy.get.and.returnValue(expectedPokemon);
+
+    service.getPokemon('ditto').subscribe(
+      pokemon => expect(pokemon).toEqual(expectedPokemon, 'expected pokemon'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
 });
+
